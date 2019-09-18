c68c16fe252cce77f46428aeb4952f847489c348
MarcoSantonastasi <marco.santonastasi@altesis-pro.com>
2019-09-18 10:25:30 +0200

    button class toggle functinality done

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,10 +1,23 @@
 class Chronometer {
-  constructor() {}
-  // startClick() {}
-  // getMinutes() {}
-  // getSeconds() {}
-  // twoDigitsNumber() {}
-  // stopClick() {}
-  // resetClick() {}
-  // splitClick() {}
+  constructor() {
+    this.currentTime = 0;
+    this.intervalId = undefined;
+  }
+  startClick(interval) {  
+    this.setIntervalId = setInterval(() => this.currentTime++, interval);
+  }
+  getMinutes() {
+    return parseInt(this.currentTime/60).toString();
+  }
+  getSeconds() {
+    return parseInt(this.currentTime%60).toString();
+  }
+  twoDigitsNumber(numberString) {
+    if (numberString.length < 2) return "0" + numberString; 
+  }
+  stopClick() {}
+  resetClick() {
+    this.currentTime = 0;
+  }
+  splitClick() {}
 }
